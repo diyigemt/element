e1398e3b54ddddfd3168a9f0c3759560834521b8
diyigemt <qwe1355247243@gmail.com>
2020-10-06 15:01:07 +0800

    import element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <el-button>to home</el-button>
     </div>
     <router-view/>
   </div>
