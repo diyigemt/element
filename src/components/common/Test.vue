20a1564697efb3659bf5d2efd4f63dc19dec046b
diyigemt <qwe1355247243@gmail.com>
2020-10-23 09:33:47 +0800

    完成合计行

--- src/components/common/Test.vue
+++ src/components/common/Test.vue
@@ -3,7 +3,7 @@
            ref="dynamicValidateForm"
            label-width="100px"
            class="demo-dynamic">
-    <el-table stripe empty-text="空" :data="orderForm.goods">
+    <el-table stripe empty-text="空" :data="orderForm.goods" show-summary :summary-method="getSummaries">
       <el-table-column label="编号" type="index" width="50px" align="center"></el-table-column>
       <el-table-column label="商品">
         <template slot-scope="scope">
@@ -19,13 +19,12 @@
                   :value="item.id">
               </el-option>
             </el-select>
-            <!--          <el-button @click.prevent="removeDomain(e)">删除</el-button>-->
           </el-form-item>
         </template>
       </el-table-column>
-      <el-table-column label="单价" width="50px">
+      <el-table-column label="单价" width="70px">
         <template slot-scope="scope">
-          <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price}}</span>
+          <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price + '元'}}</span>
         </template>
       </el-table-column>
       <el-table-column label="数量" align="center">
@@ -33,13 +32,18 @@
           <el-input-number v-model.number="orderForm.goods[scope.$index].count" :min="1"></el-input-number>
         </template>
       </el-table-column>
-      <el-table-column label="金额">
+      <el-table-column label="金额" width="100px">
         <template slot-scope="scope">
-          <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price * orderForm.goods[scope.$index].count}}</span>
+          <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price * orderForm.goods[scope.$index].count + '元'}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="right">
+        <template slot-scope="scope">
+          <el-button type="danger" @click.prevent="removeGood(orderForm.goods[scope.$index])">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-form-item>
+    <el-form-item style="margin-top: 10px;">
       <el-button type="primary" @click="submitForm('dynamicValidateForm')">提交</el-button>
       <el-button @click="addGood">新增条目</el-button>
       <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
@@ -79,7 +83,7 @@ export default {
       this.$refs[formName].resetFields();
       this.orderForm.goods = [{id: 1, value: '产品1', key: Date.now(), count: 1}]
     },
-    removeDomain(item) {
+    removeGood(item) {
       let index = this.orderForm.goods.indexOf(item)
       if (index !== -1) {
         this.orderForm.goods.splice(index, 1)
@@ -92,6 +96,10 @@ export default {
         key: Date.now(),
         count: 1
       });
+    },
+    getSummaries({columns, data}) {
+      const value = data.map(item => this.goodsList[item.id - 1].price * item.count).reduce((perv, curr) => perv + curr)
+      return ['', '', '', '总价:', value.toString() + '元', ''];
     }
   }
 }
