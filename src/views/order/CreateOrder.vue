114d1bea1c9a38f759d952bd3a8f113ccfa6e1ee
diyigemt <qwe1355247243@gmail.com>
2020-10-24 13:38:06 +0800

    完成双向绑定 等等完成回车更改

--- src/views/order/CreateOrder.vue
+++ src/views/order/CreateOrder.vue
@@ -28,63 +28,67 @@
           <span class="inline-span">当前积分: {{selectedUser.points}}</span>
         </div>
       </div>
-      <div>
-        <el-form label-position="left"
-                 ref="dynamicValidateForm"
-                 label-width="100px"
-                 class="demo-dynamic">
-          <el-table stripe empty-text="空" :data="orderForm.goods" show-summary :summary-method="getSummaries">
-            <el-table-column label="编号" type="index" width="50px" align="center"></el-table-column>
-            <el-table-column label="商品">
-              <template slot-scope="scope">
-                <el-form-item>
-                  <el-select
-                      v-model="orderForm.goods[scope.$index].id"
-                      filterable
-                      placeholder="请输入关键词">
-                    <el-option
-                        v-for="(item, index) in goodsList"
-                        :key="item.id + index"
-                        :label="item.name"
-                        :value="item.id">
-                    </el-option>
-                  </el-select>
-                </el-form-item>
-              </template>
-            </el-table-column>
-            <el-table-column label="编码">
-              <template slot-scope="scope">
-                <el-input v-model.number="orderForm.goods[scope.$index].code"></el-input>
-<!--                <span>{{goodsList[orderForm.goods[scope.$index].id - 1].code}}</span>-->
-              </template>
-            </el-table-column>
-            <el-table-column label="单价" width="70px">
-              <template slot-scope="scope">
-                <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price + '元'}}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="数量" align="center">
-              <template slot-scope="scope">
-                <el-input-number v-model.number="orderForm.goods[scope.$index].count" :min="1"></el-input-number>
-              </template>
-            </el-table-column>
-            <el-table-column label="金额" width="100px">
-              <template slot-scope="scope">
-                <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price * orderForm.goods[scope.$index].count + '元'}}</span>
-              </template>
-            </el-table-column>
-            <el-table-column align="right">
-              <template slot-scope="scope">
-                <el-button type="danger" @click.prevent="removeGood(orderForm.goods[scope.$index])">删除</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-          <el-form-item style="margin-top: 10px;">
-            <el-button type="primary" @click="submitForm('dynamicValidateForm')">提交</el-button>
-            <el-button @click="addGood">新增条目</el-button>
-            <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
+      <div style="text-align: right;">
+        <el-form inline>
+          <el-form-item label="商品">
+            <el-select
+                v-model="goodTmp.id"
+                filterable
+                placeholder="请输入关键词"
+                @change="handleChange">
+              <el-option
+                  v-for="(item, index) in goodsList"
+                  :key="item.id + index"
+                  :label="item.name"
+                  :value="item.id">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="编码">
+            <el-input clearable v-model.number="goodTmp.code" @input="handleInput"></el-input>
           </el-form-item>
         </el-form>
+      </div>
+      <div>
+        <el-table stripe empty-text="空" :data="orderForm.goods" show-summary :summary-method="getSummaries">
+          <el-table-column label="编号" type="index" width="50px" align="center"></el-table-column>
+          <el-table-column label="商品">
+            <template slot-scope="scope">
+              <span>{{goodsList[orderForm.goods[scope.$index].id - 1].name}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="编码">
+            <template slot-scope="scope">
+              <span>{{mapping[orderForm.goods[scope.$index].id]}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="单价" width="70px">
+            <template slot-scope="scope">
+              <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price + '元'}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="数量" align="center" width="150px">
+            <template slot-scope="scope">
+              <el-input-number size="small" v-model.number="orderForm.goods[scope.$index].count"
+                               :min="1"></el-input-number>
+            </template>
+          </el-table-column>
+          <el-table-column label="金额" width="100px">
+            <template slot-scope="scope">
+              <span>{{goodsList[orderForm.goods[scope.$index].id - 1].price * orderForm.goods[scope.$index].count + '元'}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="right">
+            <template slot-scope="scope">
+              <el-button type="danger" @click.prevent="removeGood(orderForm.goods[scope.$index])">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div style="margin-top: 10px;">
+          <el-button type="primary" @click="submitForm()">提交</el-button>
+          <el-button @click="addGood">新增条目</el-button>
+          <el-button @click="resetForm()">重置</el-button>
+        </div>
       </div>
     </div>
   </div>
@@ -97,6 +101,10 @@ import {confirmBox} from "@/common/utils";
 import {tmpUserList} from "@/config/tmp-config";
 import {tmpGoodsList} from "@/config/tmp-config";
 
+let tmpMapping = []
+for (let item of tmpGoodsList) {
+  tmpMapping[item.id] = item.code;
+}
 export default {
   name: "CreateOrder",
   components: {PageHeader},
@@ -110,11 +118,15 @@ export default {
       orderForm: {
         goods: [{
           id: 1,
-          code: 13177421234,
           key: Date.now(),
           count: 1,
         }]
       },
+      goodTmp: {
+        id: 1,
+        code: 13177421234
+      },
+      mapping: tmpMapping,
       goodsList: tmpGoodsList
     };
   },
@@ -144,7 +156,7 @@ export default {
         this.userList = []
       }
     },
-    submitForm(formName) {
+    submitForm() {
       //TODO submit
       let content = `确认创建?\n
                      用户名: `;
@@ -171,9 +183,8 @@ export default {
         });
       })
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-      this.orderForm.goods = [{id: 1, code: 13177421234, key: Date.now(), count: 1}]
+    resetForm() {
+      this.orderForm.goods = [{id: 1, key: Date.now(), count: 1}]
     },
     removeGood(item) {
       let index = this.orderForm.goods.indexOf(item)
@@ -184,7 +195,6 @@ export default {
     addGood() {
       this.orderForm.goods.push({
         id: 1,
-        code: 13177421234,
         key: Date.now(),
         count: 1
       });
@@ -192,6 +202,21 @@ export default {
     getSummaries({columns, data}) {
       const value = data.map(item => this.goodsList[item.id - 1].price * item.count).reduce((perv, curr) => perv + curr)
       return ['', '', '', '总价:', value.toString() + '元', ''];
+    },
+    handleChange() {
+      this.goodTmp.code = this.mapping[this.goodTmp.id] || -1;
+    },
+    handleInput(val) {
+      let length = val.toString().length;
+      let num = Number(val);
+      if (length === 11) {
+        for (let item of this.goodsList) {
+          if (item.code === num) {
+            this.goodTmp.id = item.id;
+            break;
+          }
+        }
+      }
     }
   },
   beforeRouteEnter(to, from, next) {
